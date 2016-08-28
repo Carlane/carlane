661f38d6aa9547ed2f1e3af235ac329d427da483
enggaloksingh <aloksingh.itbhu@gmail.com>
2016-08-28 22:29:26 +0530

    Signed-off-by: enggaloksingh <aloksingh.itbhu@gmail.com>

--- Universal/views.py
+++ Universal/views.py
@@ -528,7 +528,7 @@ def initreq(request , pk , format = None):
     time_slot = TimeSlot.objects.get(id = request_slot_id)
     print('finding ongoing requests')
     existing_requests = Request.objects.filter(user_id = who_requested , current_status__id__lte=9)
-    if existing_requests is not None && len(existing_requests) > 0:
+    if existing_requests is not None and len(existing_requests) > 0:
         print('on going request placed',len(existing_requests))
         return Response({'response':[{'error':True,'reason':'You already have an OnGoing Request','success':False,'id':pk }]} , status = status.HTTP_201_CREATED)
 
